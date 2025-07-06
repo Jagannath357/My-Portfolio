51da3b76e2ff8a196677b93d0f465bd35dda02b0
Jagannath357 <padhijaga200@gmail.com>
2025-07-06 23:22:58 +0530

    files uploaded

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import Profile from '../assets/images/Jagannath.jpg'
+function Home() {
+    return ( 
+        <div className="hero w-100">
+            <div className="container" >
+                <div className="container row" id='profileContainer'>
+                    <div className="col-10 d-flex flex-column align-items-start gap-2 hero-desc">
+                        <h1>Hii! I'm <span className="text-warning">Jagannath Padhi</span></h1>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti aliquid rem numquam</p>
+                        <div className="d-flex touch">
+                            <button type="button" className="btn btn-warning btn">Vew My Work</button>
+                            <button type="button" className="btn btn-outline-light ms-sm-5" id='mom'>Get In Touch</button>
+                        </div>
+                    </div>
+                    <div className="col-sm-2" id='profileImage'>
+                        <img src={Profile} alt="Profile Image" className='w-100 h-100 rounded-circle' />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
